8922d86c036279be1437d0226b4067646552bf03
Ali Baha <alibahaabadii@gmail.com>
2021-01-04 16:02:27 +0330

    add resource class to manage ui

--- app/src/main/java/com/basalam/basalamproduct/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/basalam/basalamproduct/viewmodel/ProductViewModel.kt
@@ -54,7 +54,6 @@ class ProductViewModel(
 
                             }
                         }
-
                     }
                     is DataState.Empty -> {
                         productData.postValue(Resource.Empty())
